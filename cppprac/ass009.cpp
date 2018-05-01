7a2d8af05c20d9a2f56efc1353ad9da018e56959
Hongjeongmin <jungmin1237@pusan.ac.kr>
2018-05-01 23:35:36 +0900

    4th ver of 9th assign

--- cppprac/ass009.cpp
+++ cppprac/ass009.cpp
@@ -3,98 +3,82 @@
 using namespace std;
 
 class point {
-	int x = 0, y = 0;
+	int x, y;
 public:
-	point() {}
-	point(int _x, int _y): x(_x), y(_y) {}
-	void setx(int _x) {
+	point(): x(0), y(0) {}
+	point(int _x, int _y) : x(_x), y(_y) {}
+	point(const point& p) : x(p.x), y(p.y) {}
+	void set(int _x, int _y) {
 		x = _x;
-	}
-	void sety(int _y) {
 		y = _y;
 	}
 	int read_x() const {
+		// cout << "in point class : " << x << endl;
 		return x;
 	}
 	int read_y() const {
+		// cout << "in point class : " << y << endl;
 		return y;
 	}
 };
 
 class rectangle {
 	point *bottomleft, *upperright;
-	bool isEmpty = true;
+	bool isEmpty;
 public:
 	rectangle() {
-		point bl; // bottomleft initialised
-		point ur; // upperright initialised
+		isEmpty = true;
+		point bl;
+		point ur;
+		bottomleft = &bl;
+		upperright = &ur;
 	}
 	rectangle(int x1, int x2, int y1, int y2) {
-		point bl(x1, y1);
-		point ur(x2, y2);
+		isEmpty = false;
+		bottomleft->set(x1, y1);
+		upperright->set(x2, y2);
 	}
+
 	rectangle(point bl, point ur) {
+		isEmpty = false;
 		bottomleft = &bl;
 		upperright = &ur;
 	}
 
-	int length() const {
-		int x1 = bottomleft->read_x();
-		int x2 = upperright->read_x();
-
-		return x2 - x1;
+	bool getEmpty() const {
+		return isEmpty;
 	}
-
-	int height() const {
-		int y1 = bottomleft->read_y();
-		int y2 = upperright->read_y();
-
-		return y2 - y1;
+	int blxcor() const { // x1
+		return bottomleft->read_x();
 	}
-
-	int setxcor(const rectangle& r) {
-		int setx = 0;
-		int setblx = bottomleft->read_x();
-		int seturx = upperright->read_x();
-
-		return setx;
+	int blycor() const { // y1
+		return bottomleft->read_y();
 	}
-
-	int setycor(const rectangle& r) {
-		int sety = 0;
-
-		return sety;
+	int upxcor() const { // x2
+		return upperright->read_x();
+	}
+	int upycor() const { // y2
+		return upperright->read_y();
 	}
 
 	rectangle operator+(const rectangle& r) {
 
 		int sblx = 0, surx = 0, sbly = 0, sury = 0;
 
-		int blx = bottomleft->read_x();
-		int bly = bottomleft->read_y();
-		int urx = upperright->read_x();
-		int ury = upperright->read_y();
+		int blx = blxcor();
+		int bly = blycor();
+		int urx = upxcor();
+		int ury = upycor();
 
-		int r_blx = (r.bottomleft)->read_x();
-		int r_bly = (r.bottomleft)->read_y();
-		int r_urx = (r.upperright)->read_x();
-		int r_ury = (r.upperright)->read_y();
+		int r_blx = r.blxcor();
+		int r_bly = r.blycor();
+		int r_urx = r.upxcor();
+		int r_ury = r.upycor();
 
-		if (r_blx >= blx && r_bly >= bly) {
-			sblx = blx;
-			sbly = bly;
-		} else if (r_blx < blx && r_bly < bly) {
-			sblx = r_blx;
-			sbly = r_bly;
-		}
-
-		if (r_urx >= urx && r_ury >= ury) {
-			surx = r_urx;
-			sury = r_ury;
-		} else if (r_urx < urx && r_ury < ury) {
-			surx = urx;
-			sury = ury;
-		}
+		sblx = r_blx > blx ? blx : r_blx;
+		sbly = r_bly > bly ? bly : r_bly;
+		surx = r_urx >= urx ? r_urx : urx;
+		sury = r_ury >= ury ? r_ury : r_bly;
 
 		rectangle result(sblx, surx, sbly, sury);
 		return result;
@@ -105,51 +89,86 @@ public:
 		rectangle result;
 		int sblx = 0, surx = 0, sbly = 0, sury = 0;
 
-		int blx = bottomleft->read_x();
-		int bly = bottomleft->read_y();
-		int urx = upperright->read_x();
-		int ury = upperright->read_y();
+		int blx = blxcor();
+		int bly = blycor();
+		int urx = upxcor();
+		int ury = upycor();
 
-		int r_blx = (r.bottomleft)->read_x();
-		int r_bly = (r.bottomleft)->read_y();
-		int r_urx = (r.upperright)->read_x();
-		int r_ury = (r.upperright)->read_y();
+		int r_blx = r.blxcor();
+		int r_bly = r.blycor();
+		int r_urx = r.upxcor();
+		int r_ury = r.upycor();
 
 		if (urx - r_blx >= 0 || blx - r_urx >= 0) {
-			result.isEmpty = true;
+			isEmpty = true;
+			surx = 0;
+			sury = 0;
+			sblx = 0;
+			sbly = 0;
 		}
 
 		else {
-			result.isEmpty = false;
-			int len_diff = (r.length()) - length();
-			int hei_diff = (r.height()) - height();
-
+			isEmpty = false;
+			surx = urx;
+			sury = ury;
+			sblx = r_blx;
+			sbly = r_bly;
 		}
 
-		if (result.isEmpty)
-			cout << "Empty Rectangle.\n";
+		(result.bottomleft)->set(sblx, sbly);
+		(result.upperright)->set(surx, sury);
 
 		return result;
 	}
 
 	friend istream& operator>>(istream& is, rectangle& r) {
 		int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-		cin >> x1 >> x2 >> y1 >> y2;
-		if (x1 > x2 || y1 > y2) {
-			cerr << "not available values\n";
-			return is;
-		}
-		(r.bottomleft)->setx(x1); // friend이기 때문에 r의 멤버 함수 참조 가능
-		(r.bottomleft)->sety(y1);
-		(r.upperright)->setx(x2);
-		(r.upperright)->sety(y2);
+		is >> x1 >> y1 >> x2 >> y2;
+
+		point a(x1, y1);
+		point b(x2, y2);
+
+		r = rectangle(a, b);
 		return is;
+	}
+
+	friend ostream& operator<<(ostream& os, rectangle& r) {
+		os << "bottomleft point : (" << r.blxcor() << ", " << r.blycor() << "), ";
+		os << "upperright point : (" << r.upxcor() << ", " << r.upycor() << ")\n";
+		return os;
 	}
 };
 
 
 int main()
 {
-	/* code */
+	rectangle r1, r2, r3;
+	cin >> r1;
+	cin >> r2;
+	cout << "r1 bottomleft : (" << r1.blxcor() << ", " << r1.blycor() << ")\n";
+	cout << "r1 upperright : (" << r1.upxcor() << ", " << r1.upycor() << ")\n";
+
+	cout << "r2 bottomleft : (" << r2.blxcor() << ", " << r2.blycor() << ")\n";
+	cout << "r2 upperright : (" << r2.upxcor() << ", " << r2.upycor() << ")\n";
+
+	if (r1.blxcor() > r1.upxcor() || r1.blycor() > r1.upycor()) {
+		cerr << "not available values\n";
+		return -1;
+	}
+
+	if (r2.blxcor() > r2.upxcor() || r2.blycor() > r2.upycor()) {
+		cerr << "not available values\n";
+		return -1;
+	}
+
+
+	r3 = r1 + r2;
+	cout << r3;
+	r3 = r1 - r2;
+	if (r3.getEmpty()) {
+		cerr << "Empty Rectangle\n";
+		return -1;
+	}
+	cout << r3;
 	return 0;
 }
