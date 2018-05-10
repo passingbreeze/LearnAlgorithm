22bb277f33594b0f46c6e536ce56fc51f629ee9b
passingbreeze <jungmin1237@pusan.ac.kr>
2018-05-11 02:40:14 +0900

    solving problems

--- cppprac/lkjcppassign/ass010.cpp
+++ cppprac/lkjcppassign/ass010.cpp
@@ -5,25 +5,33 @@
 using namespace std;
 
 class student {
-	student** std;
 	char name[20];
 	unsigned age;
 	char dept[20];
 	float score;
 public:
-	student(int n) {
-		std = new student*[n];
-	}
-	~student() {
-		delete std;
-	}
-
+	student(){}
 	int readFromText(char* filename, student** students) {
+		int n = 0;
 		ifstream ifs(filename);
-		// if(ifs.is_open()){
 
-		// }
+		if(!(ifs.is_open())){
+			cerr << "No Input File." << endl;
+			return -1;
+		}
 
+		ifs >> n;
+		for (int i = 0; i < n; ++i){
+			ifs.get((*students)[i].name, 20);
+			ifs.seekg(21,ifs.beg);
+			ifs>>(*students)[i].age;
+			ifs.seekg(24,ifs.beg);
+			ifs.get((*students)[i].dept, 20);
+			ifs.seekg(44,ifs.beg);
+			ifs >> (*students)[i].score;
+			cout << (*students)[i].name << ' ' << (*students)[i].age << ' ' << (*students)[i].dept << ' ' << (*students)[i].score << endl;
+		}
+		return n;
 	}
 	void writeToBinary(char* filename, int n, student* students);
 	int readFromBinary(char* filename, student** students);
@@ -33,6 +41,14 @@ public:
 
 int main()
 {
-	/* code */
+	student* std = new student[100]; // get heap address
+	int stdnum = 0;
+	char file[512] = "\0";
+	cout << "Enter Input File name : ";
+	cin >> file;
+	
+	stdnum = std->readFromText(file, &std);
+
+	delete[] std;
 	return 0;
 }
