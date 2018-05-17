daff42eb653c311e00494f75db8900eaf398eaa4
HongJeongmin <jeongmin1237@gmail.com>
2018-05-17 11:52:12 +0900

    solving problem

--- pyprac/neighbors.py
+++ pyprac/neighbors.py
@@ -1,21 +1,25 @@
-<<<<<<< HEAD
-def main():
+#-*-coding:utf-8-*-
 
+def mklist(lst):
+    evallst = lst
+    for i in range(len(evallst)):
+        for j in range(len(evallst[i])):
+            evallst[i] = evallst[i][j].split()
+    for i in range(len(evallst)):
+        for j in range(len(evallst[i])) :
+            evallst[i][j] = eval(evallst[i][j])
+    return evallst
+
+def main():
+    string = []
+    while True:
+        input_str = input().splitlines()
+        if input_str == []:
+            break
+        else :
+            string.append(input_str)
+    mat = mklist(string)
+    print(mat)
 
 if __name__ == '__main__':
     main()
-=======
-#-*-coding:utf-8-*-
-
-string = []
-
-while True:
-    input_str = input()
-    if input_str == "":
-        break
-    else:
-        string.append(input_str)
-
-for line in string:
-    print(line)
->>>>>>> 503709d48cfdd3eceee0c70c2bb7f678a8749056
