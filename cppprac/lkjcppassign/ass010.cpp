298c9eced2c89de2fa7375d3b32332ab9ea88fdf
HongJeongmin <jeongmin1237@gmail.com>
2018-05-17 13:00:23 +0900

    Solving homeworks...

--- cppprac/lkjcppassign/ass010.cpp
+++ cppprac/lkjcppassign/ass010.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstring>
 #include <sstream>
 #include <iomanip> // header for set precision
 
@@ -13,9 +12,9 @@ class student {
 	char dept[20];
 	float score;
 public:
-	student(){}
+	student() {}
 
-	int str_to_int(string s){
+	int str_to_int(string s) {
 		istringstream iss(s);
 		int n = 0;
 
@@ -23,25 +22,25 @@ public:
 		return n;
 	}
 
-	double str_to_double(string s){
+	double str_to_double(string s) {
 		istringstream iss(s);
 		iss.setf(ios::fixed | ios::showpoint);
 		double f = 0.0;
-		
+
 		iss >> setprecision(2) >> f;
-	
+
 		return f;
 	}
 
-	string int_to_str(int n){
+	string int_to_str(int n) {
 		ostringstream oss;
-	
+
 		oss << n;
 
 		return oss.str();
 	}
 
-	string float_to_str(float f){
+	string float_to_str(float f) {
 		ostringstream oss;
 		oss.setf(ios::fixed | ios::showpoint);
 
@@ -51,90 +50,90 @@ public:
 
 	int readFromText(char* filename, student** students) {
 		int n = 0;
-		
+
 		ifstream ifs(filename);
 
-		if(!(ifs.is_open())){
+		if (!(ifs.is_open())) {
 			cerr << "No Input File." << endl;
 			return -1;
 		}
 		ifs >> n; // the number of records
-	
+
 		for (int i = 0; i < n; ++i)
 			ifs >> (*students)[i];
 		ifs.close();
 		return n;
 	}
 
-	void writeToBinary(char* filename, int n, student* students){
+	void writeToBinary(char* filename, int n, student* students) {
 		ofstream ofs(filename, ios::binary);
 		// in Ubuntu Linux / Max OS X, "ifs.binary" can be used instead of "ios::binary".
-		
-		if(!(ofs.is_open())){
+
+		if (!(ofs.is_open())) {
 			cerr << "No Output File." << endl;
 			return;
 		}
 
 		ofs << n;
-		for (int i = 0; i < n; ++i){
-			ofs << students[i];
+		for (int i = 0; i < n; ++i) {
+			ofs << students[i] << endl;
 		}
 		ofs.close();
 	}
 
-	int readFromBinary(char* filename, student** students){
+	int readFromBinary(char* filename, student** students) {
 		int n = 0;
-		
-		ifstream ifs(filename, ios::binary); 
+
+		ifstream ifs(filename, ios::binary);
 		// in Ubuntu Linux / Max OS X, "ifs.binary" can be used instead of "ios::binary".
-		
-		if(!(ifs.is_open())){
+
+		if (!(ifs.is_open())) {
 			cerr << "No Input File." << endl;
 			return -1;
 		}
 		ifs >> n; // the number of records
-	
+
 		for (int i = 0; i < n; ++i)
 			ifs >> (*students)[i];
 		ifs.close();
 		return n;
 	}
 
-	void writeToText(char* filename, int n, student* students){
+	void writeToText(char* filename, int n, student* students) {
 		ofstream ofs(filename);
 
-		if(!(ofs.is_open())){
+		if (!(ofs.is_open())) {
 			cerr << "No Output File." << endl;
 			return;
 		}
 
 		ofs << n;
-		for (int i = 0; i < n; ++i){
-			ofs << students[i];
+		for (int i = 0; i < n; ++i) {
+			ofs << students[i] << endl;
 		}
 		ofs.close();
 	}
 
-	friend istream& operator>>(istream& is, student& s){
-		char std_age[3], std_score[4];
-		is.read(s.name, 20);	
+	friend istream& operator>>(istream& is, student& s) {
+		char std_age[3], std_score[6];
+		is.read(s.name, 20);
 		is.read(std_age, 3);
 		s.age = (unsigned)(s.str_to_int(std_age));
 		is.read(s.dept, 20);
-		is.read(std_score, 5);
+		is.read(std_score, 6);
 		s.score = (float)(s.str_to_double(std_score));
 		return is;
 	}
 
-	friend ostream& operator<<(ostream& os, student& s){
+	friend ostream& operator<<(ostream& os, student& s) {
 		string strage, strscore;
-		
+
 		strage = (s.int_to_str(s.age));
 		strscore = (s.float_to_str(s.score));
 		os.write(s.name, 20);
-		os.write(&strage[0], 4);
+		os.write(&strage[0], 3);
 		os.write(s.dept, 20);
-		os.write(&strscore[0],5);
+		os.write(&strscore[0], 6);
 		return os;
 	}
 };
@@ -142,7 +141,7 @@ public:
 int main()
 {
 	student* std = new student[100]; // get heap address
-	int stdnum1 = 0, stdnum2=0;
+	int stdnum1 = 0, stdnum2 = 0;
 	char txtfile[512] = "\0";
 	char binfile[512] = "\0";
 
--- pyprac/neighbors.py
+++ pyprac/neighbors.py
@@ -1,6 +1,19 @@
 #-*-coding:utf-8-*-
 
-def mklist(lst):
+def vacantmatrix(r,c):
+    m = [[0]*c for i in range(r)]
+    return m
+
+def transpose(mat):
+    row = len(mat)
+    col = len(mat[0])
+    mat2 = vacantmatrix(col, row)
+    for i in range(row):
+        for j in range(col):
+            mat2[j][i] = mat[i][j]
+    return mat2
+
+def mkevalmat(lst):
     evallst = lst
     for i in range(len(evallst)):
         for j in range(len(evallst[i])):
@@ -10,16 +23,28 @@ def mklist(lst):
             evallst[i][j] = eval(evallst[i][j])
     return evallst
 
+def neighmat(vacmat, nummat):
+    for i in range(len(nummat)):
+        for j in range(len(nummat[i])):
+            vacmat[i][j]=nummat[i][j]
+    print(vacmat)
+    return vacmat
+
 def main():
-    string = []
+    input_num = []
     while True:
-        input_str = input().splitlines()
-        if input_str == []:
+        input_str = input()
+        if input_str == "":
             break
         else :
-            string.append(input_str)
-    mat = mklist(string)
-    print(mat)
+            str = input_str.splitlines()
+            input_num.append(str)
+    
+    mat = mkevalmat(input_num)
+    row = len(mat)
+    col = len(mat[0])
+    mat2 = vacantmatrix(row+2,col+2)
+    mat2 = neighmat(mat2, mat)
 
 if __name__ == '__main__':
     main()
