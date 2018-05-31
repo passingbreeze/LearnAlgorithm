b63f9871f6e040d4e37a3f8be8f50ddc47d01332
HongJeongmin <jeongmin1237@gmail.com>
2018-05-31 15:35:29 +0900

    for practice

--- pyprac/prac.py
+++ pyprac/prac.py
@@ -1,9 +1,25 @@
 #-*-coding:utf-8-*-
-import sys;input()
-def main():
+import sys
+import datetime
+import random
 
-    for l in sys.stdin:
-        print (sum(map(int, l.split())))
+class cal :
+    def __init__(self,lst):
+        self.a = lst[0]
+        self.b = lst[1]
+        self.c = lst[2]
+    def printcal(self):
+        print((self.a+self.b)%(self.c))
+        print((self.a%self.c + self.b%self.c) % (self.c))
+        print((self.a * self.b % self.c) % (self.c))
+        print((self.a % self.c * self.b % self.c) % (self.c))
+
+def main():
+   a = [4,5,6]
+   b = a
+   b.pop()
+   print(a, b)
+
 
 if __name__ == '__main__':
     main()
