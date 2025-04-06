2babaf38dda09783e95593831f0d3f1b625db1c3
passingbreeze <jeongmin1237@nol-universe.com>
2025-04-06 19:45:44 +0900

    fix: 2nd problem

--- python3/day01_dfs/2_network_programmers.py
+++ python3/day01_dfs/2_network_programmers.py
@@ -1,5 +1,21 @@
 from typing import *
 
+def dfs(node: int, visited: Set[int], computers: List[List[int]], n: int):
+    """
+        DFS 탐색을 통해 연결된 컴퓨터를 방문하는 함수
+
+        Args:
+            node: 현재 노드
+            visited: 방문한 노드 집합
+            computers: 컴퓨터 간의 연결 정보를 담은 2차원 배열
+            n: 컴퓨터의 개수
+    """
+    visited.add(node)
+    for i in range(n):
+        # 현재 노드와 연결되어 있고, 아직 방문하지 않은 컴퓨터라면, 재귀적으로 DFS 탐색
+        if computers[node][i] == 1 and i not in visited:
+            dfs(i, visited, computers, n)
+
 def solution(n: int, computers: List[List[int]]) -> int:
     """
         DFS를 활용하여 네트워크 개수를 찾는 함수
@@ -14,19 +30,11 @@ def solution(n: int, computers: List[List[int]]) -> int:
     visited = set()
     answer = 0
 
-    # DFS를 통해 연결된 모든 컴퓨터를 방문하는 함수
-    def dfs(node):
-        visited.add(node)
-        for i in range(n):
-            # 현재 노드와 연결되어 있고, 아직 방문하지 않은 컴퓨터라면
-            if computers[node][i] == 1 and i not in visited:
-                dfs(i)
-
     # 모든 컴퓨터에 대해 확인
     for i in range(n):
         # 아직 방문하지 않은 컴퓨터라면 새로운 네트워크 시작
         if i not in visited:
-            dfs(i)
+            dfs(i, visited, computers, n)
             answer += 1
 
     return answer
